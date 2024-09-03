fbcd3613244e98c531fc1e48493ddfc039da59e9
Soap <50261583+23Soap@users.noreply.github.com>
2024-09-02 23:58:16 -0400

    Add files via upload

---
+++ 17-Arrays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // arrays = a variable that can store multiple values. fixed size
+
+            // string[] cars = { "BMW","Ferrari","McLaren" };
+
+            String[] cars = new string[3];
+
+            cars[0] = "Mercedes";
+            cars[1] = "Ferrari";
+            cars[2] = "McLaren";
+
+            for (int i =0; i < cars.Length;i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
