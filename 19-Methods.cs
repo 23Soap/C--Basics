a85b890a1cd9d7e77c70e06e948edfc289f17a21
Soap <50261583+23Soap@users.noreply.github.com>
2024-09-02 23:59:24 -0400

    Add files via upload

---
+++ 19-Methods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //method = performs a section of code, whenever its called "invoked"
+            //benefit = lets us reuse code w/o writing it multiple times.
+
+            string name = "Yusuf";
+            int age = 21;
+            singHappyBirthday(name,age);
+
+            Console.ReadKey();
+        }
+        static void singHappyBirthday(string name,int age) 
+        {
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine("Happy birthday dear " + name);
+            Console.WriteLine("You are " + age + "years old");
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Foreach loop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // foreach loop = a simpler way to iterate over an array, but its LESS fixeible
+
+            String[] cars = new string[3];
+
+            cars[0] = "Mercedes";
+            cars[1] = "Ferrari";
+            cars[2] = "McLaren";
+
+
+            foreach(string car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
